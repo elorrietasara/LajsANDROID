3880fa92512084b3df77c24345183b3afc08978c
enekobs98 <enekoburon98@gmail.com>
2020-01-09 12:37:30 +0100

    mapa

--- app/src/main/java/com/example/reservas/MainActivity.java
+++ app/src/main/java/com/example/reservas/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Adapter;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,4 +35,24 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new MyAdapter(mDataset);
         recyclerView.setAdapter(mAdapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mimenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mapa:
+
+                Intent intent = new Intent(this, MapsActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
